8af03511783bf2d9520538d5be545a520131c1d3
Vignesh Rao <svignesh1793@gmail.com>
2025-08-28 06:04:23 -0500

    chore: Create a new hello world project in XCode

---
+++ Jarvis/Jarvis/JarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisApp.swift
+//  Jarvis
+//
+//  Created by Vignesh Rao on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct JarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
